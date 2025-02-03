ffcc5125073a65d42d590db54da6e6595d936aef
enxbaatarluvsanjamts <enxbaatarluvsanjamts@gmail.com>
2025-02-03 23:00:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function sendToMessenger() {
     const message = "Tulip Tulip"; // Facebook Messenger руу илгээх текст
 
     // Messenger руу илгээх URL
-    const messengerUrl = `https://m.me/your-messenger-id?text=${encodeURIComponent(message)}`;
+    const messengerUrl = `https://m.me/Tulip Tulip?text=${encodeURIComponent(message)}`;
 
     // Шинэ таб нээнэ
     window.open(messengerUrl, '_blank');
